141e050ed56a31178f317b456e4063f860750ec2
Sam Saccone <samccone@gmail.com>
2012-12-23 12:59:16 -0500

    extract routing into a separate file

--- templates/new_express/app.js
+++ templates/new_express/app.js
@@ -31,7 +31,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+routes.set(app);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
