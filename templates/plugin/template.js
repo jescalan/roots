bb026f278377e5baf6b75d865e71f08dbee1638d
Jeff Escalante <hello@jenius.me>
2013-05-21 19:38:54 -0400

    remove blarg multipass testing directory, update plugin template to new format

--- templates/plugin/template.js
+++ templates/plugin/template.js
@@ -1,39 +1,36 @@
 // roots plugin template
 // ---------------------
 
-// if you are using external dependencies, you must use `module.require()`, as this file
+// If you are using external dependencies, you must use `module.require()`, as this file
 // will be executed in roots' environment.
 
-// you must exports a settings object in order for the compiler to be used
+// Required for compiler to run.
+// @param {string} file_type - File extension compiled by this plugin
+// @param {string} target - File extension output from this plugin
 exports.settings = {
   file_type: 'xxx',
   target: 'css'
 }
 
-// the compile method is passed a list of files of the file type specified in the above
-// settings object, a helper class, and a callback. the callback must be called only
-// once all files have been compiled
-exports.compile = function(file, Helper, callback){
-  var error = false;
+// Compiles a single file
+// @param {object} file - An object containing lots of information about the file
+// @param {function} callback - Execute when finished. Takes error and output.
+exports.compile = function(file, callback){
+  var compiled_contents, error = false;
 
-  // the helper has a ton of useful methods for managing the file paths, contents,
-  // and writing the file to the right place. pass the constructor a file.
-  // full helper documentation is available at http://roots.cx//plugins
-  var helper = new Helper(file);
+  // This is a good way to find out what's in the file object ; )
+  console.log(file);
 
-  // do your compiler thing here. if this is an async function, it is your responsability
-  // to manage it and hit the callback at the right time. this example is assumed to be sync.
+  // This compiler just converts everything to uppercase
+  // Yours probably actually does something useful.
   try {
-    var compiled_contents = helper.file_contents;
-  } catch (err) {
-    error = err;
+    compiled_contents = file.contents.toUpperCase();
+  } catch(err){
+    error = err
   }
 
-  // helper.write will write a string to the right file in /public, compressing it when
-  // necessary. I prefer not to write files when there's an error.
-  !error && helper.write(compiled_contents);
-  
-  // when all files are finished compiling, hit the callback. if you want roots to
-  // handle your errors, pass a string with the error (or false if no errors).
-  callback(error);
+  // Hit the callback only when the compile is finished. If you are compiling
+  // html and want it to work with roots' layout system, you must return a 
+  // function here rather than a string.
+  callback(error, compiled_contents);
 }
