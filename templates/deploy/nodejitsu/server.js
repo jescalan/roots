4ce6a041f61fe00dde9e0d80a417c9d69821cd16
Sean Lang <slang800@gmail.com>
2013-08-07 07:31:05 -0500

    abstracting all printing

--- templates/deploy/nodejitsu/server.js
+++ templates/deploy/nodejitsu/server.js
@@ -1,8 +1,9 @@
-var connect = require('connect');
+var connect = require('connect'),
+    roots = require('../../../index');
 
 var app = connect()
   .use(connect.logger('dev'))
   .use(connect.static('public'))
   .listen(process.env.PORT || 3000);
 
-console.log("-- Server started on port " + (process.env.PORT || 3000) + " --");
+roots.print.log("-- Server started on port " + (process.env.PORT || 3000) + " --");
